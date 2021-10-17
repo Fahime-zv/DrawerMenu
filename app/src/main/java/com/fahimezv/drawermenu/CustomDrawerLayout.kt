daa4f01a628904e9cecd807bce6342cef3837be4
fahimegit config --global user.name fahimegit config --global user.name fahime <fahime.zv@gmail.com>
2021-10-17 14:39:42 +0330

    fix bug in drawer close

--- app/src/main/java/com/fahimezv/drawermenu/CustomDrawerLayout.kt
+++ app/src/main/java/com/fahimezv/drawermenu/CustomDrawerLayout.kt
@@ -4,18 +4,15 @@ import android.content.Context
 import android.view.MotionEvent
 import android.view.View
 import androidx.drawerlayout.widget.DrawerLayout
-import java.util.ArrayList
+import java.util.*
 
 
 class CustomDrawerLayout(context:Context):DrawerLayout(context) {
-	private val directionRight = false
+	private val directionRight = true
+
 	override fun onTouchEvent(ev:MotionEvent):Boolean {
 		super.onTouchEvent(ev)
 		return true
-	}
-
-	override fun onInterceptTouchEvent(ev:MotionEvent):Boolean {
-		return super.onInterceptTouchEvent(ev)
 	}
 
 	override fun dispatchTouchEvent(ev:MotionEvent):Boolean {
@@ -28,21 +25,14 @@ class CustomDrawerLayout(context:Context):DrawerLayout(context) {
 		}
 		else {
 			if(isDrawerOpen(getChildAt(1)) || isDrawerVisible(getChildAt(1))) {
-				return if(ev.x >= getChildAt(1).left) {
-					false
-				}
-				else (ev.x <= getChildAt(1).left && ev.y >= height - getChildAt(1).left * 4f / 5f).not()
+				return ev.x < getChildAt(1).right
 			}
 		}
 		return super.dispatchTouchEvent(ev)
 	}
 
-	override fun addTouchables(views:ArrayList<View>) {
-		super.addTouchables(views)
-	}
 
-	override fun requestDisallowInterceptTouchEvent(disallowIntercept:Boolean) {
-		super.requestDisallowInterceptTouchEvent(disallowIntercept)
-	}
+
+
 
 }
--- app/src/main/java/com/fahimezv/drawermenu/MainActivity.kt
+++ app/src/main/java/com/fahimezv/drawermenu/MainActivity.kt
@@ -3,105 +3,132 @@ package com.fahimezv.drawermenu
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.ListPopupWindow
-import android.widget.Toast
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 
 class MainActivity:AppCompatActivity() {
-	private lateinit var rootFrameLayout:FrameLayout
-	private lateinit var mainFrame:CardView
-	private lateinit var secondFrame:LinearLayout
+	private lateinit var rootLayout:FrameLayout
+	private lateinit var containerFrame:CardView
+	private lateinit var menuLayout:LinearLayout
 	private lateinit var customDrawerLayout:CustomDrawerLayout
 	private lateinit var actionBarDrawerToggle:ActionBarDrawerToggle
 	private lateinit var item:MenuItem
+	private lateinit var openMenuButton:Button
 
 	@SuppressLint("ResourceType")
 	override fun onCreate(savedInstanceState:Bundle?) {
 		super.onCreate(savedInstanceState)
-		customDrawerLayout = CustomDrawerLayout(this)
-		customDrawerLayout.setScrimColor(Color.TRANSPARENT)
-		item = MenuItem(this) //		cardView = CardView(this)
-
-		rootFrameLayout = FrameLayout(this).apply {
+		rootLayout = FrameLayout(this).apply {
 			setBackgroundColor(Color.parseColor("#c1805e"))
-			item.addMenuItem(R.drawable.ic_launcher_background ,"Home" ,true) {
-				Toast.makeText(this@MainActivity ,"item1" ,Toast.LENGTH_SHORT).show()
 
-			}
-			item.addMenuItem(R.drawable.ic_launcher_background ,"Category" ,true) {
-				Toast.makeText(this@MainActivity ,"item2" ,Toast.LENGTH_SHORT).show()
-			}
-
-			item.addMenuItem(R.drawable.ic_launcher_background ,"Track Order" ,true) {
-				Toast.makeText(this@MainActivity ,"item3" ,Toast.LENGTH_SHORT).show()
-			}
-			item.addMenuItem(R.drawable.ic_launcher_background ,"Contact us " ,true) {
-				Toast.makeText(this@MainActivity ,"item4" ,Toast.LENGTH_SHORT).show()
-
-			}
+			item = createItemMenu()
 			addView(item ,ListPopupWindow.MATCH_PARENT ,ViewGroup.LayoutParams.MATCH_PARENT)
 
+			customDrawerLayout = createCustomDrawerLayout()
+
+			addView(
+				customDrawerLayout ,ViewGroup.LayoutParams(
+					ViewGroup.LayoutParams.MATCH_PARENT ,ViewGroup.LayoutParams.MATCH_PARENT
+														  )
+				   )
 
 		}
 
-		setContentView(rootFrameLayout)
+		setContentView(rootLayout)
 
-		rootFrameLayout.addView(
-			customDrawerLayout ,ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT ,ViewGroup.LayoutParams.MATCH_PARENT)
-							   )
 
-		mainFrame = CardView(this).apply {
-			id = 1235
+
+		containerFrame = createContainerFrame().also {
+			it.addView(openMenuButton ,WRAP_CONTENT ,WRAP_CONTENT)
 		}
+
 		supportFragmentManager.commit {
 			setReorderingAllowed(true)
 			replace<MainFragment>(1235)
 
 		}
 
+		menuLayout = createMenuLayout()
+		customDrawerLayout.also {
+			val params1 = DrawerLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT ,ViewGroup.LayoutParams.MATCH_PARENT)
+			it.addView(containerFrame ,params1)
 
+			val params2 = DrawerLayout.LayoutParams(500 ,ViewGroup.LayoutParams.MATCH_PARENT)
+			params2.gravity = Gravity.LEFT
 
+			it.addView(menuLayout ,params2)
 
-		secondFrame = LinearLayout(this).apply {
-			gravity = Gravity.CENTER_VERTICAL
-			orientation = LinearLayout.VERTICAL
-			setBackgroundColor(Color.TRANSPARENT)
+			actionBarDrawerToggle = createActionBarDrawerToggle()
+			it.addDrawerListener(actionBarDrawerToggle)
 		}
 
-		val params1 = DrawerLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT ,ViewGroup.LayoutParams.MATCH_PARENT)
-		customDrawerLayout.addView(mainFrame ,params1)
-		val params2 = DrawerLayout.LayoutParams(500 ,ViewGroup.LayoutParams.MATCH_PARENT)
-		params2.gravity = Gravity.LEFT
-		customDrawerLayout.addView(secondFrame ,params2)
 
+	}
 
-		actionBarDrawerToggle = object:ActionBarDrawerToggle(this ,customDrawerLayout ,R.string.app_name ,R.string.app_name) {
+	private fun createMenuLayout() = LinearLayout(this).apply {
+		gravity = Gravity.CENTER_VERTICAL
+		orientation = LinearLayout.VERTICAL
+		setBackgroundColor(Color.TRANSPARENT)
 
-			override fun onDrawerSlide(drawerView:View ,slideOffset:Float) {
-				super.onDrawerSlide(drawerView ,slideOffset)
-				val slideX = drawerView.width * slideOffset
-				mainFrame.scaleX = 1 - (slideOffset / 10f);
-				mainFrame.scaleY = 1 - (slideOffset / 10f);
-				mainFrame.translationX = slideX
-				mainFrame.radius = slideOffset * 80
+	}
 
-			}
+	@SuppressLint("ResourceType")
+	private fun createContainerFrame() = CardView(this).apply {
+		id = 1235            //OpenCloseMenu
+		openMenuButton = createButtonOpenMenu()
+
+	}
+
+	private fun createCustomDrawerLayout() = CustomDrawerLayout(this@MainActivity).apply {            //shadow
+		setScrimColor(Color.TRANSPARENT)
+	}
+
+	private fun createItemMenu() = MenuItem(this).apply {
+		addMenuItem(R.drawable.ic_launcher_background ,"Home" ,true) {
+			Toast.makeText(this@MainActivity ,"item1" ,Toast.LENGTH_SHORT).show()
+		}
+		addMenuItem(R.drawable.ic_launcher_background ,"Category" ,true) {
+			Toast.makeText(this@MainActivity ,"item2" ,Toast.LENGTH_SHORT).show()
+		}
+
+		addMenuItem(R.drawable.ic_launcher_background ,"Track Order" ,true) {
+			Toast.makeText(this@MainActivity ,"item3" ,Toast.LENGTH_SHORT).show()
+		}
+		addMenuItem(R.drawable.ic_launcher_background ,"Contact us " ,true) {
+			Toast.makeText(this@MainActivity ,"item4" ,Toast.LENGTH_SHORT).show()
 
 
 		}
-		customDrawerLayout.addDrawerListener(actionBarDrawerToggle)
+	}
 
 
+	private fun createButtonOpenMenu() = Button(this).apply {
+		text = "Open/close Menu"
+		setOnClickListener {
+			if(customDrawerLayout.isDrawerOpen(GravityCompat.START)) customDrawerLayout.closeDrawer(GravityCompat.START)
+			else customDrawerLayout.openDrawer(GravityCompat.START)
+		}
+	}
+
+	private fun createActionBarDrawerToggle() = object:ActionBarDrawerToggle(this ,customDrawerLayout ,R.string.app_name ,R.string.app_name) {
+		override fun onDrawerSlide(drawerView:View ,slideOffset:Float) {
+			super.onDrawerSlide(drawerView ,slideOffset)
+			val slideX = drawerView.width * slideOffset
+			containerFrame.scaleX = 1 - (slideOffset / 10f);
+			containerFrame.scaleY = 1 - (slideOffset / 10f);
+			containerFrame.translationX = slideX
+			containerFrame.radius = slideOffset * 80
+
+		}
 	}
 }
